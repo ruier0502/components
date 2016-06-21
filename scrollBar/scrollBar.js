c9cbac881e4e116f9b5182569f8766bc66104ea0
liuxuerui <ruier0502@126.com>
2016-06-21 11:35:03 +0800

    解决this指向问题题，将事件定义在函数原型中

--- scrollBar/scrollBar.js
+++ scrollBar/scrollBar.js
@@ -19,26 +19,68 @@ scrollBar.prototype.init = function(initEle){
 
 };
 scrollBar.prototype.sliderHeight = function(){
-    // body... 
+    // body...
     var _this = this;
     var scrollClientH = _this.scrollContentEle.clientHeight;
     var scrollHeight = _this.scrollContentEle.scrollHeight;
     _this.scrollBarSliderEle.style.height = scrollClientH/scrollHeight * scrollClientH + 'px';
 };
 scrollBar.prototype.action = function(argument){
-     // body... 
-    var _this = this;
+     // body...
+     var _this = this;
     if(_this.getBrowserVersion() == "firefox") {
-        _this.scrollContentEle.addEventListener('DOMMouseScroll', _this.mousewheel, false);
+        _this.scrollContentEle.addEventListener('DOMMouseScroll', mousewheel, false);
     }
     else {
-        _this.scrollContentEle.onmousewheel = _this.mousewheel;
+        _this.scrollContentEle.onmousewheel = mousewheel;
     }
     if(document.addEventListener) {
-        _this.scrollBarSliderEle.addEventListener('mousedown', _this.mousedown);
+        _this.scrollBarSliderEle.addEventListener('mousedown', mousedown);
     }
     else if(document.attachEvent) {
-        _this.scrollBarSliderEle.attachEvent('mousedown', _this.mousedown);
+        _this.scrollBarSliderEle.attachEvent('mousedown', mousedown);
+    }
+
+    // 以下是动作函数
+    function mousedown(event) {
+        var evt = event||window.event;
+        _this.scrollPageY = evt.clientY;
+        _this.scrollY = _this.scrollContentEle.scrollTop;
+        document.body.onselectstart = function() {
+            return false;
+            // 避免拖动时引起的文字被选中的现象
+        }
+        document.documentElement.onmousemove = mousemove;
+        document.documentElement.onmouseup = mouseup;
+    }
+    function mouseup(event) {
+        document.documentElement.onmousemove = null;
+    }
+    function mousemove(event) {
+        var evt = event || window.event;
+        var step = _this.scrollContentEle.scrollHeight/_this.scrollContentEle.clientHeight;
+        _this.scrollContentEle.scrollTop =_this.scrollY + step*(evt.clientY - _this.scrollPageY);
+        _this.setSliderPosition();
+    }
+    function mousewheel(event) {
+        var per = 20;
+        var event = event || window.event;
+        var wheelDelta = event.wheelDelta? event.wheelDelta/120 : event.detail/3;
+        if(_this.getBrowserVersion() == "firefox") {
+            if(wheelDelta > 0)
+                this.scrollTop += per;
+            else
+                this.scrollTop -= per;
+        }
+        else{
+            if(wheelDelta < 0) {
+                this.scrollTop += per;
+            }
+            else {
+                this.scrollTop -= per;
+            }
+        }
+        _this.setSliderPosition();
     }
     
 };
@@ -97,47 +139,4 @@ scrollBar.prototype.findChild = function(parentEle, className) {
             return childNode;
     }
     return false;
-}
-scrollBar.prototype.mousedown = function(event) {
-    var _this = scrollBar;
-    var evt = event||window.event;
-    _this.scrollPageY = evt.clientY;
-    _this.scrollY = _this.scrollContentEle.scrollTop;
-    document.body.onselectstart = function() {
-        return false;
-        // 避免拖动时引起的文字被选中的现象
-    }
-    document.documentElement.onmousemove = mousemove;
-    document.documentElement.onmouseup = mouseup;
-}
-scrollBar.prototype.mouseup = function(event) {
-    document.documentElement.onmousemove = null;
-}
-scrollBar.prototype.mousemove = function(event) {
-    var _this = scrollBar;
-    var evt = event || window.event;
-    var step = _this.scrollContentEle.scrollHeight/_this.scrollContentEle.clientHeight;
-    _this.scrollContentEle.scrollTop =_this.scrollY + step*(evt.clientY - _this.scrollPageY);
-    _this.setSliderPosition();
-}
-scrollBar.prototype.mousewheel = function(event) {
-    var _this = scrollBar;
-    var per = 20;
-    var event = event || window.event;
-    var wheelDelta = event.wheelDelta? event.wheelDelta/120 : event.detail/3;
-    if(_this.getBrowserVersion() == "firefox") {
-        if(wheelDelta > 0)
-            this.scrollTop += per;
-        else
-            this.scrollTop -= per;
-    }
-    else{
-        if(wheelDelta < 0) {
-            this.scrollTop += per;
-        }
-        else {
-            this.scrollTop -= per;
-        }
-    }
-    _this.setSliderPosition();
 }
